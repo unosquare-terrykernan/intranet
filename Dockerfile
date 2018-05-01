c644dbf1a1e104a48ceafbeefffe9dbe8646e156
trojanspike <v3047-github@yahoo.co.uk>
2018-05-01 15:42:17 +0100

    commit changes

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,10 @@
 FROM wordpress:php7.2-apache
 
 COPY ./wordpress /var/www/html
+RUN chown -R www-data:www-data /var/www/html
 
 RUN apt-get update \
-	&& apt-get install -y --no-install-recommends git-core \
+	&& apt-get install -y --no-install-recommends git-core vim \
 	&& apt-get -y clean \
 	&& rm -rf /var/lib/apt/lists/*
 
@@ -15,6 +16,7 @@ RUN {\
 RUN a2enmod cgi
 
 # docker build --rm -t unosquare/intranet:0.0.0 .
+# docker run -d -p 80:80 unosquare/intranet:0.0.0
 # docker run -d -e MYSQL_USER=user -e MYSQL_PASSWORD=password -e MYSQL_DATABASE=database  -e MYSQL_ROOT_PASSWORD=password --name wordpress-mariadb mariadb:5.5
 # docker run --rm -ti -v C:\Users\leemkay\Development\Unosquare\intranet:/var/www:www-data:www-data -p 80:80 --link wordpress-mariadb:mariadb unosquare/intranet:0.0.0
 # docker run --rm -ti --link wordpress-mariadb:mariadb unosquare/intranet:0.0.0
--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -46,14 +46,14 @@ define('DB_COLLATE', '');
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         'R85QyiKWdtJTNws9tx');
-define('SECURE_AUTH_KEY',  'yPBw7pSF1gpj3IMY3h');
-define('LOGGED_IN_KEY',    '5JrPJ3Nwb6ccb61k3v');
-define('NONCE_KEY',        'PkNdvsCpX8Ib65Q2De');
-define('AUTH_SALT',        'Q6ySUBaUIv2jfIPWN2');
-define('SECURE_AUTH_SALT', 'HxWazutbTAJSb5MPg8');
-define('LOGGED_IN_SALT',   'teSOnra4X8vhZNETLc');
-define('NONCE_SALT',       'euW5eSMnWx7er9uW4F');
+define('AUTH_KEY',         '[VDov6_W/mgYAPQ.MABYWzee?Y}C1klu%X_BMgoXT:Yj=BdQ[r*q/%JMT7oGFDz_');
+define('SECURE_AUTH_KEY',  '6}^L8(?4|QR}O5W+Mwh$8te=$3|21u&mW0guLkI{i0fSq^KvtCEu G{SX`/[#3+!');
+define('LOGGED_IN_KEY',    '[[gI3]qLFpxHiC63qSqIPB$j8/d-1[Hh1pcsgRPOVt_t9WtIBe*R?f2I]5j <VWV');
+define('NONCE_KEY',        '[4M,qYVWFi/zdR:=.,w<RanKW!*51&bM`)9~a/(iDj3r_?LBHXIAU8Q*gzY|&fmm');
+define('AUTH_SALT',        '*o%ZC@mmpY23}DTXn9o/m#bIGw,D1Yi8UR7[;2TuQ$80:t$>4T0nB`E6/9{^ ( $');
+define('SECURE_AUTH_SALT', 'Fh$)7XIr0NAS:y1qVD2jMWTnxs0o1Sm7:___e6/5aOv{?XX80jUKrGPUqS2,b9!#');
+define('LOGGED_IN_SALT',   ']W%I#l8E:%U/RoRSI2(-];H=MhIJ1!`yAijBd!Q%|X=pO_DG5e+|.Z~ d*1?A_,Q');
+define('NONCE_SALT',       'uON )e$1Ep|L4^ThF4v$#Rbvelc[1opz(P*muh;Vg9CdGL#BlZGbAC9@M}<n%B]c');
 
 /**#@-*/
 
