e2addc2b9b0220c72bb6318ce52475a0da279751
trojanspike <v3047-github@yahoo.co.uk>
2018-05-02 15:18:02 +0100

    edit wp-config issue

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -46,14 +46,14 @@ define('DB_COLLATE', '');
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         '[VDov6_W/mgYAPQ.MABYWzee?Y}C1klu%X_BMgoXT:Yj=BdQ[r*q/%JMT7oGFDz_';
-define('SECURE_AUTH_KEY',  "6}^L8(?4|QR}O5W+Mwh$8te=$3|21u&mW0guLkI{i0fSq^KvtCEu G{SX`/[#3+!";
-define('LOGGED_IN_KEY',    '[[gI3]qLFpxHiC63qSqIPB$j8/d-1[Hh1pcsgRPOVt_t9WtIBe*R?f2I]5j <VWV';
-define('NONCE_KEY',        (isset($_ENV['NONCE_KEY']))?$_ENV['NONCE_KEY']:'put your unique phrase here');
-define('AUTH_SALT',        (isset($_ENV['AUTH_SALT']))?$_ENV['AUTH_SALT']:'put your unique phrase here');
-define('SECURE_AUTH_SALT', (isset($_ENV['SECURE_AUTH_SALT']))?$_ENV['SECURE_AUTH_SALT']:'put your unique phrase here');
-define('LOGGED_IN_SALT',   (isset($_ENV['LOGGED_IN_SALT']))?$_ENV['LOGGED_IN_SALT']:'put your unique phrase here');
-define('NONCE_SALT',       (isset($_ENV['NONCE_SALT']))?$_ENV['NONCE_SALT']:'put your unique phrase here');
+define('AUTH_KEY',         '[VDov6_W/mgYAPQ.MABYWzee?Y}C1klu%X_BMgoXT:Yj=BdQ[r*q/%JMT7oGFDz_');
+define('SECURE_AUTH_KEY',  '6}^L8(?4|QR}O5W+Mwh$8te=$3|21u&mW0guLkI{i0fSq^KvtCEu G{SX`/[#3+!');
+define('LOGGED_IN_KEY',    '[[gI3]qLFpxHiC63qSqIPB$j8/d-1[Hh1pcsgRPOVt_t9WtIBe*R?f2I]5j <VWV');
+define('NONCE_KEY',        '[4M,qYVWFi/zdR:=.,w<RanKW!*51&bM`)9~a/(iDj3r_?LBHXIAU8Q*gzY|&fmm');
+define('AUTH_SALT',        '*o%ZC@mmpY23}DTXn9o/m#bIGw,D1Yi8UR7[;2TuQ$80:t$>4T0nB`E6/9{^ ( $');
+define('SECURE_AUTH_SALT', 'Fh$)7XIr0NAS:y1qVD2jMWTnxs0o1Sm7:___e6/5aOv{?XX80jUKrGPUqS2,b9!#');
+define('LOGGED_IN_SALT',   ']W%I#l8E:%U/RoRSI2(-];H=MhIJ1!`yAijBd!Q%|X=pO_DG5e+|.Z~ d*1?A_,Q');
+define('NONCE_SALT',       'uON )e$1Ep|L4^ThF4v$#Rbvelc[1opz(P*muh;Vg9CdGL#BlZGbAC9@M}<n%B]c');
 
 /**#@-*/
 
